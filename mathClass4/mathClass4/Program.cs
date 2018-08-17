5a173c094dde7aba2bcc15840135597fcbdb8203
AJLevine <37715494+AJLevine@users.noreply.github.com>
2018-08-17 14:52:23 -0700

    Add files via upload

---
+++ mathClass4/mathClass4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mathClass4
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            //     2. In the Main() method of the console app, instantiate the class.
+            mathOperation firstAttempt = new mathOperation();
+
+            //     3. Ask the user to input two numbers, one at a time.Let them know they need not enter anything for the second number.
+            Console.WriteLine("Input two numbers, one at a time.  The second number is optional: ");
+            int firstParam = Convert.ToInt32(Console.ReadLine());
+            int secondParam = Convert.ToInt32(Console.ReadLine());
+
+            mathOperation.Taketwo(firstParam, secondParam);
+
+            Console.WriteLine(firstParam);
+
+            Console.ReadKey();
+         }
+    }
+
+}
+        
+
