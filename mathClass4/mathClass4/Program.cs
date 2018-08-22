e43ad16714da7cefd118d486e2b138101f551706
AJLevine <37715494+AJLevine@users.noreply.github.com>
2018-08-22 16:43:03 -0700

    Add files via upload

--- mathClass4/mathClass4/Program.cs
+++ mathClass4/mathClass4/Program.cs
@@ -12,16 +12,15 @@ namespace mathClass4
         {
 
             //     2. In the Main() method of the console app, instantiate the class.
-            mathOperation firstAttempt = new mathOperation();
+            MathOperation firstAttempt = new MathOperation();
 
             //     3. Ask the user to input two numbers, one at a time.Let them know they need not enter anything for the second number.
             Console.WriteLine("Input two numbers, one at a time.  The second number is optional: ");
             int firstParam = Convert.ToInt32(Console.ReadLine());
             int secondParam = Convert.ToInt32(Console.ReadLine());
 
-            mathOperation.Taketwo(firstParam, secondParam);
-
-            Console.WriteLine(firstParam);
+            Console.WriteLine("You will now receive the first integer with an operation performed on it: ");
+            firstAttempt.Taketwo(firstParam, secondParam);
 
             Console.ReadKey();
          }
--- mathClass4/mathClass4/mathOperation.cs
+++ mathClass4/mathClass4/mathOperation.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace mathClass4
 {
-    class mathOperation
+    public class MathOperation
     {
+        //     1.   Create a class. In that class, create a method that takes two integers as parameters.
+        //          Make one of them optional.Have the method do a math operation and return a integer result.
+        
+
+        public void Taketwo (int firstParam, int secondParam)
+        {
+            firstParam = firstParam * 5;
+            Console.WriteLine(firstParam);
+            
+        }
+
+        
+        //     4. Call the method in the class, passing in the one or two numbers entered.
+        
     }
 }
