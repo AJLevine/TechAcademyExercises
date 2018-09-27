39602db63a23c53dae9c1bf07c491a2b8cfbcb51
AJLevine <37715494+AJLevine@users.noreply.github.com>
2018-09-27 12:14:01 -0700

    Add files via upload

---
+++ EmployeeList1/EmployeeList1/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeList1
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName()
+        {
+            Console.WriteLine("NAME: " + FirstName + " " + LastName);
+        }
+    }
+}
