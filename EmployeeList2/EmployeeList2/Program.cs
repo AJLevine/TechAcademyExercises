213e3f63ec3e98bef937121d2c7965ba098b87b7
AJLevine <37715494+AJLevine@users.noreply.github.com>
2018-10-01 11:32:01 -0700

    Add files via upload

---
+++ EmployeeList2/EmployeeList2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeList2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employees = new Employee
+            {
+                firstName = "Sample",
+                lastName = "Student"
+            };
+            employees.SayName();
+
+        }
+    }
+}
