87bb2da91206d34997da2ca59ffea8b11830a088
AJLevine <37715494+AJLevine@users.noreply.github.com>
2018-10-01 14:55:02 -0700

    Add files via upload

---
+++ EmployeeList3/EmployeeList3/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeList3
+{
+    public class Employee : Person, IQuittable
+    {
+        public override void SayName()
+        {
+            Console.WriteLine(firstName + " " + lastName);
+        }
+        public void Quit()
+        {
+            Console.WriteLine("I, " + firstName + " " +
+                lastName + ", do hereby Quit!");
+        }
+    }
+}
---
+++ EmployeeList3/EmployeeList3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeList3
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            IQuittable name = new Employee();
+            Employee employees = new Employee()
+            {
+                firstName = "Sample",
+                lastName = "Student"
+            };
+            employees.SayName();
+            name.Quit();
+
+        }
+    }
+}
